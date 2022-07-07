ef68e4bb8b629a3a3dd4b738497f710e08599b66
Rezsuta Péter <rezsutapeter@gmail.com>
2022-07-07 10:27:33 +0200

    Production class
    Production class has been created with 4 properties and the class's constructer is included,

---
+++ C#/beugro_projekt/beugro_projekt/Production.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beugro_projekt
+{
+    public class Production
+    {
+        public int Pcb_id { get; set; }
+        public int Quantity { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+
+        public Production(int Pcb_id, int Quantity, DateTime startDate, DateTime endDate)
+        {
+            this.Pcb_id = Pcb_id;
+            this.Quantity = Quantity;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+    }
+}
